a170bea3a83c18c607f6f43b4cbb322643f292ab
naushad.warsi <naushad.warsi@klingelnberg.com>
2021-01-27 20:10:05 +0530

    create a service for movie search

---
+++ Services/MoviesServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using InstemTest.Models;
+using Newtonsoft.Json;
+
+namespace InstemTest.Services
+{
+    public class MoviesServiceProvider
+    {
+        public List<MovieDataModel> GetTopFourMovies()
+        {
+            var allMovies = JsonConvert.DeserializeObject<List<MovieDataModel>>(File.ReadAllText(@"./Resouces/moviedata.json"));
+
+            var topFour = allMovies
+                .OrderByDescending(x => x.Year)
+                .Take(4)
+                .ToList();
+            return topFour;
+        }
+    }
+}

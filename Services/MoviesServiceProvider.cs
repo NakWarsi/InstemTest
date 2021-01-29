4f8e3573aa4e627d2b946509e4fb3cdaf2990932
naushad.warsi <naushad.warsi@klingelnberg.com>
2021-01-29 18:34:31 +0530

    create search machanism

--- Services/MoviesServiceProvider.cs
+++ Services/MoviesServiceProvider.cs
@@ -27,11 +27,45 @@ namespace InstemTest.Services
 
         public List<MovieDataModel> SearchMovies(string searchSting)
         {
-            var topFour = _allMovies
-                .OrderByDescending(x => x.Year)
-                .Take(4)
-                .ToList();
-            return topFour;
+            List<MovieDataModel> matchedMovie = new List<MovieDataModel>();
+            foreach (var row in _allMovies)
+            {
+                if (IfPatternExist(searchSting,row.Title))
+                {
+                    matchedMovie.Add(row);
+                }
+            }
+            return matchedMovie;
+        }
+
+        private bool IfPatternExist(string patternArg, string txtArg)
+        {
+
+            var txt = txtArg.ToUpper();
+            var pattern = patternArg.ToUpper();
+            int patternLength = pattern.Length;
+            int textLength = txt.Length;
+            int textIndex = 0;
+
+            while (textIndex <= textLength - patternLength)
+            {
+                int currentIndex;
+                for (currentIndex = 0; currentIndex < patternLength; currentIndex++)
+                {
+                    if (txt[textIndex + currentIndex] != pattern[currentIndex])
+                        break;
+                }
+
+                if (currentIndex == patternLength)
+                {
+                    return true;
+                }
+                else if (currentIndex == 0)
+                    textIndex = textIndex + 1;
+                else
+                    textIndex = textIndex + currentIndex;
+            }
+            return false;
         }
     }
 }

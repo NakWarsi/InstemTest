6e8d8ba484a0c0c778a9f553ac25fe7ec60ab5d5
NakWarsi <warsinak@gmail.com>
2021-01-30 20:47:00 +0530

    register Service

--- InstemTest.HttpApi/Startup.cs
+++ InstemTest.HttpApi/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InstemTest.Service;
 
 namespace InstemTest.HttpApi
 {
@@ -25,6 +26,7 @@ namespace InstemTest.HttpApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<MovieManagerService>();
             services.AddControllers();
         }
 

2dd756264b6dcb53943d302730a484d88400342c
naushad.warsi <naushad.warsi@klingelnberg.com>
2021-01-30 18:33:30 +0530

    restructure the project

---
+++ InstemTest_Tests/PatternMatcherService_Test.cs
@@ -0,0 +1,35 @@
+﻿using InstemTest.Services;
+using NUnit.Framework;
+
+namespace InstemTest_Tests
+{
+    class PatternMatcherService_Test
+    {
+        private PatternMatcherService matcherObj;
+        [SetUp]
+        public void Setup()
+        {
+            matcherObj = new PatternMatcherService();
+        }
+
+        [Test]
+        public void TestPatternMatherServiceWithValidSubString()
+        {
+            Assert.IsTrue(matcherObj.IfPatternExist("a", "abc"));
+            Assert.IsTrue(matcherObj.IfPatternExist("ab", "abc"));
+            Assert.IsTrue(matcherObj.IfPatternExist("abc", "abc"));
+            Assert.IsTrue(matcherObj.IfPatternExist("b", "abc"));
+            Assert.IsTrue(matcherObj.IfPatternExist("bc", "abc"));
+            Assert.IsTrue(matcherObj.IfPatternExist("c", "abc"));
+        }
+
+        [Test]
+        public void TestPatternMatherServiceWithInvalidSubString()
+        {
+            Assert.IsFalse(matcherObj.IfPatternExist("d", "abc"));
+            Assert.IsFalse(matcherObj.IfPatternExist("cb", "abc"));
+            Assert.IsFalse(matcherObj.IfPatternExist("g", "abc"));
+            Assert.IsFalse(matcherObj.IfPatternExist("ca", "abc"));
+        }
+    }
+}

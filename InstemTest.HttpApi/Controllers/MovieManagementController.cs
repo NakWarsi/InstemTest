3385e825de504a0f485a7a8d45f8551f099aa154
naushad.warsi <naushad.warsi@klingelnberg.com>
2021-01-30 19:08:00 +0530

    update controller

--- InstemTest.HttpApi/Controllers/MovieManagementController.cs
+++ InstemTest.HttpApi/Controllers/MovieManagementController.cs
@@ -9,16 +9,16 @@ namespace InstemTest.HttpApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class MovieManagementController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<MovieManagementController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public MovieManagementController(ILogger<MovieManagementController> logger)
         {
             _logger = logger;
         }
